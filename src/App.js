0c90e94c2a921afaf39500b4315a4826727d890e
ifty17 <iftyrahatkabir@gmail.com>
2022-10-03 12:41:30 +0600

    added route

--- src/App.js
+++ src/App.js
@@ -15,11 +15,16 @@ function App() {
       {path: '/' , element: <Home></Home>},
       {path: '/home' , element: <Home></Home>},
       {path: '/products' , element: <Products></Products>},
-      {path: 'friends' , element: <Friends></Friends>},
+      {path: 'friends' ,
+      loader: () =>{
+        return fetch("https://jsonplaceholder.typicode.com/users");
+      },
+       element: <Friends></Friends>},
 
     ]},
 
     {path: '/about' , element: <About></About>},
+    {path: '*', element: <div>This route not found</div>}
   ]);
 
   return (
